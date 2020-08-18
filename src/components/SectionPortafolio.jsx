dbc07e450929ce829d39c77da0875ef87d1f925c
edwin.sanchez <edwin.s.g@hotmail.com>
2020-08-18 12:19:47 -0500

    borre el hola

--- src/components/SectionPortafolio.jsx
+++ src/components/SectionPortafolio.jsx
@@ -6,7 +6,7 @@ const SectionPortafolio = () =>(
   <div className="section__container">
     <div className="section__texto">
       <h1>Portafolio</h1>
-      El repositorio de este sitio se encuentra ubicado en Github hola:
+      El repositorio de este sitio se encuentra ubicado en Github :
       <li> <a href="https://github.com/edwin21991/ProgramadorAutodidacta" target="_blank">Programador Autodidacta</a></li><br/>
       App creada en React Native ubicada en el siguiente repositorio de Github:
       <li><a href="https://github.com/edwin21991/Trailers.git" target="_blank">App Trailler</a></li>
